cc4af0f57f905653764450ef8cfd4055d458bbbf
unknown <1807244619@qq.com>
2024-01-11 21:24:27 +0800

    Initial commit
    Generated by react-native@0.72.7

---
+++ js/babel.config.js
@@ -0,0 +1,19 @@
+"use strict";
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: ['.js', '.json'],
+                alias: {
+                    '@': './src',
+                },
+            },
+        ],
+        'inline-dotenv',
+        'react-native-reanimated/plugin', // needs to be last
+    ],
+};
